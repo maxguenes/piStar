126d5f53637db1fc4da20319e0fd5509d6bcb6cb
Max Guenes <maxguenes@gmail.com>
2017-07-04 01:56:32 -0300

    C9 sample application URL

--- tool/istarcore/repository.js
+++ tool/istarcore/repository.js
@@ -2,5 +2,5 @@
  * Created by maxguenes on 18/06/2017.
  */
 
-var PI_STAR_URL = "http://localhost:8080";
+var PI_STAR_URL = "http://pistar-backend-maxguenes.c9users.io:8081";
 var USE_REPOSITORY = true;
